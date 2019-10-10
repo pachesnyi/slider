a8e5896a7954806979d9138314358911c44ec6ca
houdini <houdini@templatemonster.me>
2019-10-10 23:55:52 +0300

    ADD: setbacgkround function

--- script.js
+++ script.js
@@ -1,10 +1,45 @@
 (function(){
 
+    var sliders = [];
+
     function MySlider(element) {
         if (!(element instanceof HTMLElement)) {
             throw new Error('element should be HTMLElement');
         }
+
+
+        this._element = element;
+        this._slide = element.querySelector('.houdini-slide');
+        this._slides = element.querySelectorAll('.houdini-slide');
+        this._slidesAmount = this._slides.length;
+        this._currentSlide = 0;
+
+        if(!this._slides) {
+            throw new Error('.houdini-slide has to be included into slider');
+        }
+
+        if(this._slide.hasAttribute("data-slide-bg")) {
+            this._slides.forEach(element => {
+                this.setSlideBg(element, element.getAttribute("data-slide-bg"));
+            });
+        }
+
     }
+
+    MySlider.prototype.setSlideBg = function (el, value) {
+        if(!el) {
+            throw new Error('slide bg is not defined');
+        }
+
+        el.style.cssText = `background-image: url('images/${value}'); background-size: cover;`;
+    };
+
+    document.addEventListener( 'DOMContentLoaded', function () {
+        document.querySelectorAll('.houdini-slider').forEach((slider)=> {
+          sliders.push(new MySlider(slider));
+        });
+        
+      });
 
 
 })();
